2b539121f3158a8dabd2eea17250ba3284043f30
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-07-17 23:32:02 +1000

    Add dependencies

---
+++ Backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app =  express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors());
+
+
+export default app;
