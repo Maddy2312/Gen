8e95ab1fd8c2c7c98a698717e906610e43a1763f
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-07-21 18:32:33 +1000

    feat: initialize App component with router provider and auth state synchronization

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import "./App.css"
 import { RouterProvider } from 'react-router'
 import { routes } from './app.routes'
+import useAuth from '../features/auth/hooks/useAuth.js'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const App = () => {
+  const { handleGetUser } = useAuth();
+  const user = useSelector((state) => state.auth.user);
+  useEffect(() => {
+    handleGetUser();
+  }, []);
+  console.log(user);
   return (
     <RouterProvider router={routes} />
   )
