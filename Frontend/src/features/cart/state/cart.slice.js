15c7b29e4573d88accd4786827f0e67ce8f3c159
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-07-31 18:55:15 +1000

    feat: initialize Redux slice for cart management with item and state reducers

---
+++ Frontend/src/features/cart/state/cart.slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    totalPrice: null,
+    currency: null,
+    items: [],
+    loading: false,
+    error: null,
+  },
+
+  reducers: {
+    setCart: (state, action) => {
+      state.totalPrice = action.payload.totalPrice;
+      state.currency = action.payload.currency;
+      state.items = action.payload.items;
+    },
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export default cartSlice.reducer;
