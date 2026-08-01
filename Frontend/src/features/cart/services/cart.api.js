dd8db7b196a99b96b2fb2c527ca4a5218cbe8aba
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-08-01 22:02:40 +1000

    feat: create cart service with add item functionality using axios instance

---
+++ Frontend/src/features/cart/services/cart.api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const cartApiInstance =
+  axios /
+  create({
+    baseURL: "http://localhost:3000/api/cart",
+    withCredentials: true,
+  });
+
+export const addItemToCart = async (productId, variantId) => {
+  try {
+    const response = await cartApiInstance.post(
+      `/add/${productId}/${variantId}`,
+      {
+        quantity: 1,
+      },
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
