57e0bbe1e170f4efa31eac7c4ff43aa97fb6142d
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-08-01 22:06:30 +1000

    feat: create useCart hook to manage adding items to the cart via Redux service

---
+++ Frontend/src/features/cart/hooks/useCart.js
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import { addItemToCart } from "../services/cart.api";
+
+
+const useCart = () => {
+    const dispatch = useDispatch();
+    const handleAddToCart = async (productId, variantId) => {
+        try {
+            const response = await addItemToCart(productId, variantId);
+            return response;
+        } catch (error) {
+            throw error;
+        }
+    };
+    return { handleAddToCart };
+}
+
+export default useCart
