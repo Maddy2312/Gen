2caace6409b1bf8b7b5d19f56f2712e567e93950
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-08-03 17:39:01 +1000

    feat: implement cart page frontend and backend routing for add and retrieve operations

--- Backend/src/routes/cart.routes.js
+++ Backend/src/routes/cart.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
 import { validateAddToCart } from "../validators/cart.validator.js";
-import { addToCart } from "../controllers/cart.controller.js";
+import { addToCart, getCart } from "../controllers/cart.controller.js";
 
 const cartRouter = Router();
 
 cartRouter.post("/add/:productId/:variantId", authenticateUser, validateAddToCart, addToCart);
+cartRouter.get("/cart", authenticateUser, getCart);
 
 export default cartRouter;
--- Frontend/src/features/cart/pages/Cart.jsx
+++ Frontend/src/features/cart/pages/Cart.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
 
-const Cart = () => {  
+const Cart = () => {
+  const cart = useSelector((state) => state.cart);
+  const { items } = cart;
+  
+
+  useEffect(() => {
+    console.log(items);
+  }, [items])
   
   return (
     <div>Cart</div>
